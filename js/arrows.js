b5ba5817ae0fe482633f36abfcf1fd8f0dc77131
srikar kaushik <srikar kaushik>
2017-02-01 11:03:34 -0600

    committing intial files

---
+++ js/arrows.js
@@ -0,0 +1,25 @@
+/*for arrows
+
+     
+          navigation : false, // Show next and prev buttons
+          slideSpeed : 300,
+          pagination:true,
+          paginationSpeed : 400,
+            // Navigation
+    navigation : true,
+    navigationText : ["<i class='fa fa-arrow-left' aria-hidden='true'></i>","<i class='fa fa-arrow-right' aria-hidden='true'></i>"],
+    rewindNav : true,
+    scrollPerPage : false,
+          autoHeight : true,
+          items:4,
+         /* itemsCustom : [
+                [0, 1],
+                [450, 2],
+                [600, 2],
+                [700, 2],
+                [1000, 4],
+                [1200, 4],
+                [1400, 4],
+                [1600, 4]
+              ],*/
+*/
